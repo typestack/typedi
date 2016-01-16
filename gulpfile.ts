50155defa1f3d178d60183b54980f014bc3461d3
Umed Khudoiberdiev <zarrhost@gmail.com>
2016-01-16 11:02:52 +0500

    small fixes

--- gulpfile.ts
+++ gulpfile.ts
@@ -76,7 +76,7 @@ export class Gulpfile {
     @Task()
     packageGenerateDts(cb: Function) {
         glob("./src/**/*.ts", (err: any, files: string[]) => {
-            const name = require(__dirname + "/package.json").name;
+            const name = require("./package.json").name;
             dtsGenerator({
                 name: name,
                 baseDir: "./src",
