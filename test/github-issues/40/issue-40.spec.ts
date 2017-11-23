fc12414b038db9002b31946c09627fcfa936103b
Umed Khudoiberdiev <zarrhost@gmail.com>
2017-11-23 14:40:49 +0500

    added test for #40

---
+++ test/github-issues/40/issue-40.spec.ts
@@ -0,0 +1,37 @@
+import "reflect-metadata";
+import {Container} from "../../../src/Container";
+import {Service} from "../../../src/decorators/Service";
+import {Inject} from "../../../src/decorators/Inject";
+import {expect} from "chai";
+
+describe("github issues > #40 Constructor inject not working", function() {
+
+    beforeEach(() => Container.reset());
+
+    it("should be able to set a class into the container", function() {
+
+        @Service("AccessTokenService")
+        class AccessTokenService {
+
+            constructor(@Inject("moment") public moment: any,
+                        @Inject("jsonwebtoken") public jsonwebtoken: any,
+                        @Inject("cfg.auth.jwt") public jwt: any) {
+            }
+
+        }
+
+        Container.set("moment", "A");
+        Container.set("jsonwebtoken", "B");
+        Container.set("cfg.auth.jwt", "C");
+        const accessTokenService = Container.get<AccessTokenService>("AccessTokenService");
+
+        expect(accessTokenService.moment).not.to.be.undefined;
+        expect(accessTokenService.jsonwebtoken).not.to.be.undefined;
+        expect(accessTokenService.jwt).not.to.be.undefined;
+
+        accessTokenService.moment.should.be.equal("A");
+        accessTokenService.jsonwebtoken.should.be.equal("B");
+        accessTokenService.jwt.should.be.equal("C");
+    });
+
+});
