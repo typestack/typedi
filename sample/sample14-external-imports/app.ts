92ce766f565454a44ca98d31d05ea35ae9d722d7
Umed Khudoiberdiev <zarrhost@gmail.com>
2018-05-08 20:48:00 +0500

    added external module usage example #65

---
+++ sample/sample14-external-imports/app.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import * as fs from "fs";
+import {Container} from "../../src";
+import {FileReader} from "./FileReader";
+
+Container.set("fs", fs);
+
+Container.get(FileReader)
+    .read(__dirname + "/text.txt")
+    .then(data => console.log(data.toString()));
