aff2542f7e81383d1c9613109fdef2825b5c7619
Attila Olah <NoNameProvided@users.noreply.github.com>
2021-03-09 08:18:56 +0100

    fix: implement AbstractConstructable correctly

--- src/types/abstract-constructable.type.ts
+++ src/types/abstract-constructable.type.ts
@@ -1,7 +1,7 @@
 /**
  * Generic type for abstract class definitions.
  *
- * Explanation: This describes a callable Function with a prototype Which is
+ * Explanation: This describes a newable Function with a prototype Which is
  * what an abstract class is - no constructor, just the prototype.
  */
-export type AbstractConstructable<T> = CallableFunction & { prototype: T };
+export type AbstractConstructable<T> = NewableFunction & { prototype: T };
