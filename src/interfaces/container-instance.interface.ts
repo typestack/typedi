25a82a183105aa09520082ba72c91fa56bb8f828
Nikolay Puliak <npulyak@remedypointsolutions.com>
2022-08-15 11:15:39 +0400

    LIB-23 remove workflow

--- src/interfaces/container-instance.interface.ts
+++ src/interfaces/container-instance.interface.ts
@@ -1,19 +1,19 @@
-import {ContainerIdentifier} from "../types/container-identifier.type";
-import {ServiceIdentifier} from "../types/service-identifier.type";
-import {ServiceOptions} from "./service-options.interface";
-import {Handler} from "./handler.interface";
+import { ContainerIdentifier } from '../types/container-identifier.type';
+import { ServiceIdentifier } from '../types/service-identifier.type';
+import { ServiceOptions } from './service-options.interface';
+import { Handler } from './handler.interface';
 
 export interface ContainerInstanceInterface {
   readonly id: ContainerIdentifier;
-  has<T = unknown>(identifier: ServiceIdentifier<T>): boolean
-  get<T = unknown>(identifier: ServiceIdentifier<T>): T
-  getMany<T = unknown>(identifier: ServiceIdentifier<T>): T[]
-  set<T = unknown>(serviceOptions: ServiceOptions<T>): this
-  remove(identifierOrIdentifierArray: ServiceIdentifier | ServiceIdentifier[]): this
-  of(containerId: ContainerIdentifier): ContainerInstanceInterface
-  findOf(containerId: ContainerIdentifier): ContainerInstanceInterface | undefined
-  registerHandler(handler: Handler): ContainerInstanceInterface
-  import(services: Function[]): ContainerInstanceInterface
-  reset(options: { strategy: 'resetValue' | 'resetServices' }): this
-  dispose(): Promise<void>
+  has<T = unknown>(identifier: ServiceIdentifier<T>): boolean;
+  get<T = unknown>(identifier: ServiceIdentifier<T>): T;
+  getMany<T = unknown>(identifier: ServiceIdentifier<T>): T[];
+  set<T = unknown>(serviceOptions: ServiceOptions<T>): this;
+  remove(identifierOrIdentifierArray: ServiceIdentifier | ServiceIdentifier[]): this;
+  of(containerId: ContainerIdentifier): ContainerInstanceInterface;
+  findOf(containerId: ContainerIdentifier): ContainerInstanceInterface | undefined;
+  registerHandler(handler: Handler): ContainerInstanceInterface;
+  import(services: Function[]): ContainerInstanceInterface;
+  reset(options: { strategy: 'resetValue' | 'resetServices' }): this;
+  dispose(): Promise<void>;
 }
