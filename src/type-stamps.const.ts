bddaad78000aa6c6ade8a55ed25b5dfb1451c70d
exascape <freshgum.bubbles@protonmail.com>
2023-06-19 22:19:29 +0100

    feat(types): Add INJECTED_FACTORY stamp

--- src/type-stamps.const.ts
+++ src/type-stamps.const.ts
@@ -4,3 +4,10 @@
  * _Note: This value is for internal use only._
  */
 export const LAZY_REFERENCE = Symbol('LAZY_REFERENCE');
+
+/**
+ * A stamp signifying that an object is a `InjectedFactory`.
+ * 
+ * _Note: This value is for internal use only._
+ */
+export const INJECTED_FACTORY = Symbol('INJECTED_FACTORY');
